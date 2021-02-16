afb511b3d5366386409a46863beb66ba39cd72bc
Sheykina Daria <71318362+DoraTheExplorer2508@users.noreply.github.com>
2021-02-16 16:05:05 +0300

    Update frequency.cpp

--- frequency.cpp
+++ frequency.cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include "generator.h"
 #define MOD 211
+#define N 100000
 
 void frequency() {
-	long count[MOD] = { 0 };
-	int counter, f1 = 1, f2 = 1, f3 = 1, f = 0, c2;
-	for (counter = 0; counter < 100000; counter++) { //генерируется массив из чисел и считаются повторения
-		f = generator(f1, f2, f3);
+	int count[MOD] = { 0 };
+	int i, m, f;
+	for (i = 0; i < N; i++) {
+		f = generator();
 		count[f]++;
-		f1 = f2;
-		f2 = f3;
-		f3 = f;
 	}
-	for (c2 = 0; c2 < MOD; c2++) {
-		printf("%d %d \n", c2, count[c2]);
+	for (m = 0; m < MOD; m++) {
+		printf("%d %d\n", m, count[m]);
 	}
 }
