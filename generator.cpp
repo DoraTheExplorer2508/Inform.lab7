a8f3e839cef277a03d93890a17874600d57048a3
Sheykina Daria <71318362+DoraTheExplorer2508@users.noreply.github.com>
2021-02-16 16:04:26 +0300

    Update generator.cpp

--- generator.cpp
+++ generator.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #define MOD 211
 
-int generator(int f1, int f2, int f3) {
-	int f = (f1 + f2 + f3) % MOD;
+int generator() {
+	static int f1 = 1, f2 = 1, f3 = 1, i, f;
+	f = (f1 + f2 + f3) % MOD;
+	f1 = f2;
+	f2 = f3;
+	f3 = f;
 	return f;
 }
